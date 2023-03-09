8b2115640cc6568408932c30b528fa8a0966e299
balacoder <balacsc51@gmail.com>
2023-03-09 23:23:29 +0530

    commit
    test

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react'
 
-import { ExampleComponent } from 'react-jsontotable'
+import { JsonTable } from 'react-jsontotable'
 import 'react-jsontotable/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  const data=[{name:'bala',age:500,address:'Paramakudi'},{name:'chandra',age:600,address:'Melur'},{name:'varun',age:700,address:'Thuraiyur'}]
+  return <>  
+  <h2 align='center'>Json Table without filter</h2>
+  <table className='timecard'>
+    <JsonTable data={data}/>
+  </table>  
+  <br/>
+  <h2 align='center'>Json Table with filter (age)</h2>
+  <table className='timecard'>
+    <JsonTable data={data} removeField={['age']}/>
+  </table>  
+  <br/>
+  </>
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -4,3 +4,32 @@ import styles from './styles.module.css'
 export const ExampleComponent = ({ text }) => {
   return <div className={styles.test}>Example Component: {text}</div>
 }
+
+export const JsonTable=({data,removeField=[]})=>{    
+  let head=[]     
+  let body=[...data]  
+  head=Object.keys(data[0]).filter(el=>!removeField.includes(el)).map(el=>el.toUpperCase().replaceAll('_',' '))
+  body=body.map(el=>
+      Object.keys(el).filter(key =>
+          removeField.every(el=>el!==key)
+          ).reduce((obj, key) =>
+          {
+              obj[key] = el[key];
+              return obj;
+          }, {}
+      )  
+  )
+  head=head.map((x,i)=><th key={i}>{x}</th>)
+  body=body.map((el,i)=>Object.keys(el).map(x=>el[x])).map((el,i)=>
+          <tr key={i}>
+              {el.map((x,i)=><td key={i}>{x}</td>)}
+          </tr>)
+return <React.Fragment>
+        <thead>
+            <tr>{head}</tr>
+        </thead>
+        <tbody>
+            {body}
+        </tbody>
+       </React.Fragment>
+}
